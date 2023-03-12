0a78d1baefcf3766cfec0ffb4387ca1c2ec84355
NickdevC <nick_coles@msn.com>
2023-03-12 13:58:47 +0000

    Install ES7 extension and explore common uses

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,60 @@
+// Typing 'rfce' gives the following snippet:
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// Typing 'rafce' gives the following snippet (same as above but with an arrow function):
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// Typing 'rce' gives the following snippet (same as first but with alternative component import syntax):
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// Typing 'rconst' gives the following constructor snippet:
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// Typing 'clg' gives the following console log snippet:
+
+// console.log(first)
+
+
+// Typing 'clo' gives the following console log (with object) snippet:
+
+// console.log('first', first)
