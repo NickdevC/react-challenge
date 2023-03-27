3e95f2efe8029d6941bdb911edd1e9610689cc3d
NickdevC <nick_coles@msn.com>
2023-03-27 15:26:54 +0000

    Create PostItem child component

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
 
 export class Content extends Component {
     constructor(props) {
@@ -14,17 +15,7 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    {savedPosts.map((post) => {
-                        return (
-                            <div className={css.SearchItem}>
-                                <p>{post.title}</p>
-                                <p>{post.name}</p>
-                                <img src={post.image} alt="random"/>
-                                <p>{post.description}</p>
-                            </div>
-                        )
-                    })
-                }
+                    <PostItem savedPosts={savedPosts} />
                 </div>
             </div>
         )
