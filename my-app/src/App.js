68dea816a5de2987c4ff4ae0c98099acd5cd2e0f
NickdevC <nick_coles@msn.com>
2023-03-27 15:07:02 +0000

    Add map function to iterate through posts list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 // import NavBarSimple from './components/NavBarSimple';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import {savedPosts} from "../posts.json";
 
 export class Content extends Component {
     constructor(props) {
@@ -9,12 +11,21 @@ export class Content extends Component {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
-
+                    <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-
+                    {savedPosts.map((post) => {
+                        return (
+                            <div className={css.SearchItem}>
+                                Individual Post
+                            </div>
+                        )
+                    })
+                }
                 </div>
             </div>
         )
     }
 }
+
+export default Content
