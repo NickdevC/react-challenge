8db90a87eb8e45e16df9e4e79b09f3687a7d71ea
NickdevC <nick_coles@msn.com>
2023-03-29 20:22:04 +0000

    Add Loader.js component

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+        
+        this.state = {
+         isLoaded: false
+      }
     }
 
     render() {
