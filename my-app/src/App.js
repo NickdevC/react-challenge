75c69fef45aa1bf2e1372aca9e7c46db0142952a
NickdevC <nick_coles@msn.com>
2023-04-10 21:04:38 +0000

    Add ContentHook.js file and convert Content function using React hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import ContentHook from './components/ContentHook';
 // import NavBarSimple from './components/NavBarSimple';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <ContentHook />
     </div>
   );
 }
