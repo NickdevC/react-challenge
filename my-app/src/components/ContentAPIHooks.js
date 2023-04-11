257e6cad451f8c8c6a2592467c2ec5600220be98
NickdevC <nick_coles@msn.com>
2023-04-11 12:41:25 +0000

    Import appropriate files for ContentAPIHooks.js

---
+++ my-app/src/components/ContentAPIHooks.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import css from "./css/Content.module.css";
+import PostItemAPI from "./PostItemAPI";
+import Loader from "./Loader";
+import axios from "axios";
+import API_KEY from "../secrets";
+
+export default function ContentAPIHooks() {
+  return (
+    <div>ContentAPIHooks</div>
+  )
+}
