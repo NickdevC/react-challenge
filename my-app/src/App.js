fd9bbc9992b6f3e55f4241a076c0ab8403a53351
NickdevC <nick_coles@msn.com>
2023-04-11 12:37:07 +0000

    Add ContentAPI.js and PostItemAPI.js with tutorial code updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,8 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
-import ContentHook from './components/ContentHook';
-// import NavBarSimple from './components/NavBarSimple';
+import ContentAPI from './components/ContentAPI';
+
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
-      <ContentHook />
+      <ContentAPI />
     </div>
   );
 }
